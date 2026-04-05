6ae4e5d2b90e0b79664cc916c2dff8dc19b2e93e
sorb852 <reeldob34@gmail.com>
2026-04-05 18:54:50 +0800

    I guess bro
    I'm pushing ig

---
+++ modules/base/theme.nix
@@ -0,0 +1,80 @@
+{ inputs, ... }:
+{
+  flake.theme =
+    let
+      inherit (inputs.nixpkgs) lib;
+      toRGB =
+        let
+          slice = p: h: builtins.substring p 2 h;
+          dec = h: lib.fromHexString h;
+        in
+        hex: {
+          r = dec (slice 1 hex);
+          g = dec (slice 3 hex);
+          b = dec (slice 5 hex);
+        };
+
+      fromRGB =
+        let
+          slice = h: builtins.substring (builtins.stringLength h - 6) 6 h;
+          f = n: lib.toHexString n;
+        in
+        {
+          r,
+          g,
+          b,
+        }:
+        "#" + slice ("000000" + (f (r * 65536 + g * 256 + b)));
+
+      brighten =
+        v: h:
+        let
+          rgb = toRGB h;
+          lerp =
+            n:
+            let
+              res = n + (builtins.floor ((255 - n) * v));
+            in
+            if res < 0 then
+              0
+            else if res > 255 then
+              255
+            else
+              0;
+        in
+        fromRGB {
+          r = lerp rgb.r;
+          g = lerp rgb.g;
+          b = lerp rgb.b;
+        };
+    in
+    {
+      shade0 = "#0c101e";
+      shade1 = "#2c303d";
+      shade2 = "#4c4f5d";
+      shade3 = "#6c6f7c";
+      shade4 = "#8c8f9b";
+      shade5 = "#acafba";
+      shade6 = "#ccceda";
+      shade7 = "#eceef9";
+
+      accent0 = "#212B52";
+      accent1 = "#202735";
+      accent2 = "#919bb1";
+      accent3 = "#afbbd6";
+      accent4 = "#3c465d";
+      accent5 = "#414971";
+      accent6 = "#3c465d";
+      accent7 = "#293256";
+
+      lib = {
+        inherit toRGB fromRGB brighten;
+      };
+    };
+  # config.colutils =
+
+  #   {
+
+  #   };
+
+}
--- modules/features/terminal.nix
+++ modules/features/terminal.nix
@@ -1,8 +1,6 @@
 { self, ... }:
 
 {
-  imports = [ ./../../utils/theme.nix ];
-
   flake.homeModules.terminal = {
     programs.kitty = {
 
@@ -16,53 +14,48 @@
         "kitty_mod+<" = "set_background_opacity -0.1";
       };
 
-      settings =
-        let
-          # colutils = self.lib.colutils;
-          # theme = (import self.lib.theme.config);
-        in
-        {
-          kitty_mod = "ctrl+shift";
+      settings = {
+        kitty_mod = "ctrl+shift";
 
-          cursor = theme.accent6;
-          cursor_text_color = theme.shade0;
-          cursor_shape = "block";
-          cursor_trail = 1;
-          cursor_trail_decay = "0.1 0.4";
+        cursor = self.theme.accent6;
+        cursor_text_color = self.theme.shade0;
+        cursor_shape = "block";
+        cursor_trail = 1;
+        cursor_trail_decay = "0.1 0.4";
 
-          scrollbar_handle_color = theme.shade1;
+        scrollbar_handle_color = self.theme.shade1;
 
-          url_color = theme.accent5;
-          url_style = "curly";
+        url_color = self.theme.accent5;
+        url_style = "curly";
 
-          background = theme.shade0;
-          foreground = theme.shade7;
+        background = self.theme.shade0;
+        foreground = self.theme.shade7;
 
-          selection_foreground = theme.shade2;
-          selection_background = theme.accent4;
+        selection_foreground = self.theme.shade2;
+        selection_background = self.theme.accent4;
 
-          # color0 = theme.shade0;
-          # color1 = theme.accent0;
-          # color2 = theme.accent3;
-          # color3 = theme.accent2;
-          # color4 = theme.accent5;
-          # color5 = theme.accent7;
-          # color6 = theme.accent4;
-          # color7 = theme.shade6;
-          #
-          # color8 = colutils.brighten 0.25 theme.shade0;
-          # color9 = colutils.brighten 0.2 theme.accent0;
-          # color10 = colutils.brighten 0.2 theme.accent3;
-          # color11 = colutils.brighten 0.2 theme.accent2;
-          # color12 = colutils.brighten 0.2 theme.accent5;
-          # color13 = colutils.brighten 0.2 theme.accent7;
-          # color14 = colutils.brighten 0.2 theme.accent4;
-          # color15 = theme.shade7;
+        color0 = self.theme.shade0;
+        color1 = self.theme.accent0;
+        color2 = self.theme.accent3;
+        color3 = self.theme.accent2;
+        color4 = self.theme.accent5;
+        color5 = self.theme.accent7;
+        color6 = self.theme.accent4;
+        color7 = self.theme.shade6;
 
-          dynamic_background_opacity = true;
-          background_opacity = 0.9;
-          background_blur = 1;
-        };
+        color8 = self.theme.lib.brighten 0.25 self.theme.shade0;
+        color9 = self.theme.lib.brighten 0.2 self.theme.accent0;
+        color10 = self.theme.lib.brighten 0.2 self.theme.accent3;
+        color11 = self.theme.lib.brighten 0.2 self.theme.accent2;
+        color12 = self.theme.lib.brighten 0.2 self.theme.accent5;
+        color13 = self.theme.lib.brighten 0.2 self.theme.accent7;
+        color14 = self.theme.lib.brighten 0.2 self.theme.accent4;
+        color15 = self.theme.shade7;
+
+        dynamic_background_opacity = true;
+        background_opacity = 0.9;
+        background_blur = 1;
+      };
     };
   };
 }
